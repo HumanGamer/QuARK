4128b8ff2dd78a3067de872d23e44cc3fee8f357
danielpharos <danielpharos@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2021-10-24 21:57:42 +0000

    Improved string formatting.

--- runtime/trunk/plugins/__init__.py
+++ runtime/trunk/plugins/__init__.py
@@ -27,7 +27,7 @@ def LoadPlugins(beginning):
         for file in nt.listdir(dir):
             f = file.upper()
             if (f[-3:]=='.PY') and (f[:len(beginning)]==beginning):
-                quarkx.log("Loading plugin: %s" % (file), LOG_VERBOSE)
+                quarkx.log("Loading plugin: %s" % (file, ), LOG_VERBOSE)
                 module = __import__(file[:-3], globals(), locals(), [])
                 if not (module in LoadedPlugins):
                     LoadedPlugins.append(module)
