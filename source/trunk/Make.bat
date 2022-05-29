1dc77a1d5876ca79f7cfa07ae12675ece596c260
danielpharos <danielpharos@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2022-05-29 15:14:07 +0000

    Oops

--- source/trunk/Make.bat
+++ source/trunk/Make.bat
@@ -4,14 +4,14 @@ ECHO Compiling QuArK.exe without debug information
 ECHO ---------------------------------------------
 ECHO.
 REM Check if compiler is available
-REM DCC32.EXE --version > NUL 2> NUL
+DCC32.EXE --version > NUL 2> NUL
 REM The ERRORLEVEL for 'file not found' is 9009
-REM IF ERRORLEVEL 9009 GOTO NoDCC32
+IF ERRORLEVEL 9009 GOTO NoDCC32
 REM B = recompile ALL units, H = show hints, W = show warnings, Q = don't list all the unit file names
-REM $D- = no debug info $L- = no local debug symbols
-REM DCC32.EXE QuArK.dpr -B -H -W -Q -$D- -$L- -$O+
+REM $D- = no debug info, $L- = no local debug symbols, $O+ = optimization
+DCC32.EXE QuArK.dpr -B -H -W -Q -$D- -$L- -$O+
 REM If the Delphi compiler didn't exit with an error level of 0, exit the batch file.
-REM IF NOT ERRORLEVEL 0 GOTO CompileFailed
+IF NOT ERRORLEVEL 0 GOTO CompileFailed
 
 ECHO.
 ECHO.
@@ -21,7 +21,7 @@ ECHO.
 REM Make Windows look for UPX in the PATH system variable, but don't display the output.
 "%ProgramFiles(x86)%\Windows Kits\10\bin\10.0.20348.0\x86\mt.exe" > NUL 2> NUL
 REM The ERRORLEVEL for 'file not found' is 9009
-REM IF ERRORLEVEL 9009 GOTO NoMT
+IF ERRORLEVEL 9009 GOTO NoMT
 "%ProgramFiles(x86)%\Windows Kits\10\bin\10.0.20348.0\x86\mt.exe" -nologo -validate_manifest -canonicalize -check_for_duplicates -manifest "QuArK.manifest" -outputresource:..\runtime\QuArK.exe
 
 ECHO.
